05fab5928ac525a1f9bacd54662d87df8599bb24
Roka <maximkalosha@gmail.com>
2026-07-03 17:14:19 +0300

    Fix: fixed a mistypos and again another function

--- ihatepdf.py
+++ ihatepdf.py
@@ -8,27 +8,35 @@ subparsers.required = True
 
 # create_pdf
 
-parser_create = subparsers.add_parser("create", help="Make PDF from a picture/a folder with pictures")
-parser_create.add_argument("input", "Path to a picture/a folder with pictures")
-parser_create.add_argument("output", "Name and path where to output pdf. Ex.: result.pdf or output/result.pdf")
+parser_create = subparsers.add_parser(
+    "create", help="Make PDF from a picture/a folder with pictures"
+)
+parser_create.add_argument("input", help="Path to a picture/a folder with pictures")
+parser_create.add_argument(
+    "output",
+    help="Name and path where to output pdf. Ex.: result.pdf or output/result.pdf",
+)
 
 # convert_pdf
 
-parser_create = subparsers.add_parser("convert", help="Convert PDF file to DOCX")
-parser_create.add_argument("input", "Path to a picture/a folder with a pdf file")
-parser_create.add_argument("output", "Name and path where to output docx. Ex.: result.docx or output/result.docx")
+parser_convert = subparsers.add_parser("convert", help="Convert PDF file to DOCX")
+parser_convert.add_argument("input", help="Path to a pdf file")
+parser_convert.add_argument(
+    "output",
+    help="Name and path where to output docx. Ex.: result.docx or output/result.docx",
+)
 
-args = parser.parse_args()
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-ihatepdf = PDF(args.input, args.output)
+    ihatepdf = PDF(args.input, args.output)
 
-if args.command == "create":
-    print("Compiling pdf file...")
-    ihatepdf.create_pdf()
-    print("Done.")
+    if args.command == "create":
+        print("Compiling pdf file...")
+        ihatepdf.create_pdf()
+        print("Done.")
 
-elif args.command == "convert":
-    print("Converting into docx...")
-    ihatepdf.convert_pdf()
-    print("Done.")
-
+    elif args.command == "convert":
+        print("Converting into docx...")
+        ihatepdf.convert_pdf()
+        print("Done.")
