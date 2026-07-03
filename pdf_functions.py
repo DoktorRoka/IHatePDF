a107b7f7d668d5079580feb24bdca9fb9148b10b
Roka <maximkalosha@gmail.com>
2026-07-03 14:38:42 +0300

    Feat: made a function to create pdf files from batch files
    Docs: updated .gitignore

--- pdf_functions.py
+++ pdf_functions.py
@@ -1,2 +1,19 @@
+import os
 import img2pdf
 
+input_files = "imgs/"
+
+
+def create_pdf(input_files):
+    temp_list = []
+
+    # files need to be processed from a folder without writing their exact names
+    for i in os.listdir(input_files):
+        temp_list.append(input_files + i)
+
+    print(temp_list)
+    with open("output/output.pdf", "wb") as f:
+        f.write(img2pdf.convert(temp_list))
+
+
+create_pdf(input_files)
