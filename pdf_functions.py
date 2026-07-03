7c5efa753fd7ebf2d930f46559e649e7369eb4f9
Roka <maximkalosha@gmail.com>
2026-07-03 16:39:21 +0300

    Feat: Drastically changed pdf functions
    Integrated all functions into a single class.
    
    Create pdf is now sorting files before making a pdf file out of them. Create pdf now checks if file name ends with .png .jpg or .jpeg. Create pdf now doesn't break if there is no picture type files.

--- pdf_functions.py
+++ pdf_functions.py
@@ -2,23 +2,35 @@ import os
 import img2pdf
 from pdf2docx import Converter
 
-input_files = "imgs/"
 
+class PDF:
+    def __init__(self, input_path, output):
+        self.input_path = input_path
+        self.output = output
 
-def create_pdf(input_files):
-    temp_list = []
+        output_dir = os.path.dirname(self.output)
 
-    # files need to be processed from a folder without writing their exact names
-    for i in os.listdir(input_files):
-        if i.endswith(".png") or i.endswith(".jpg"):
-            temp_list.append(input_files + i)
-        else:
-            continue
+        if output_dir:
+            os.makedirs(output_dir, exist_ok=True)
 
-    with open("output/output.pdf", "wb") as f:
-        f.write(img2pdf.convert(temp_list))
+    def create_pdf(self):
+        temp_list = []
 
+        # files need to be processed from a folder without writing their exact names
+        for i in sorted(os.listdir(self.input_path)):
+            if i.lower().endswith((".png", ".jpg", ".jpeg")):
+                temp_list.append(os.path.join(self.input_path, i))
+            else:
+                continue
 
-def noOCR_pdf(input_pdf):
-    cv = Converter(input_pdf)
-    cv.convert("output/output.docx")
+        if not temp_list:
+            print("Empty folder, no picture type files.")
+            return
+
+        with open(self.output, "wb") as f:
+            f.write(img2pdf.convert(temp_list))
+
+    def convert_pdf(self):
+        cv = Converter(self.input_path)
+        cv.convert(self.output)
+        cv.close()
